7ec21172b85960ea0728613c09b12a8f427ffee1
Kyeongsoo Choi <mandoo180@gmail.com>
2024-10-23 02:41:43 +0900

    fix: brightnessctl added, renaming things

--- install-packages.sh
+++ install-packages.sh
@@ -3,14 +3,20 @@
 # essentials
 sudo pacman -Syu
 sudo pacman -S git vim openssh coreutils bluez bluez-utils networkmanager
-sudo pacman -S `awk '{printf("%s ", $1)}' apps.txt`
+sudo pacman -S `awk '{printf("%s ", $1)}' packages.txt`
 
 # yay setup
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd -
-yay --version
+if [ -f "$(which yay)" ]; then
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd -
+    yay --version
+fi
+
+# install packages from aur (arch user repository)
+yay -Syu
+yay -S `awk '{printf("%s ", $1)}' packages-aur.txt`
 
 # bluetooth setup
 sudo systemctl enable --now bluetooth
