d75607257e921d679e4bac9fce520095e0f66730
Kyeongsoo Choi <mandoo180@gmail.com>
2024-11-04 01:58:39 +0900

    fix: gnome setup added

---
+++ setup-gaming-env.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+flatpak install steam
+# 20) app/com.valvesoftware.Steam/x86_64/stable 
+flatpak install steam
+# 6) runtime/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable
+flatpak install lutris
+flatpak install flathub com.github.Matoking.protontricks
+flatpak install flathub net.davidotek.pupgui2
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install gamescope
+# 3) runtime/org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08
+flatpak install mangohud
+# 5) runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08
